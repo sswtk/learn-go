9d44f20dd41fd51f06d062d5b6c5c3bdf3401774
RoninssWang <sswss5@aliyun.com>
2022-02-24 07:50:14 +0800

    action: implement main.go and todo

--- project-jk/bookstore/cmd/bookstore/main.go
+++ project-jk/bookstore/cmd/bookstore/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"bookstore/store/factory"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
+func main() {
+	s, err := factory.New("memo") // 创建图书存储模块实例
+	if err != nil {
+		panic(err)
+	}
+
+	srv := server.NewBookStoreServer(":8080", s) // 创建http服务实例
+
+	errChan, err := srv.ListenAndServe() //运行http服务
+	if err != nil {
+		log.Println("web server start failed:", err)
+		return
+	}
+	log.Println("web server start ok")
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+
+	select { //监听来自errChan以及c的事件
+	case err = <-errChan:
+		log.Println("web server run failed:", err)
+		ctx, cf := context.WithTimeout(context.Background(), time.Second)
+		defer cf()
+		err = srv.Shutdown(ctx) //优雅关闭http服务
+	}
+	if err != nil {
+		log.Println("bookstore program exit error", err)
+		return
+	}
+	log.Println("bookstore program exit ok")
 }
